01fce0698146977b2b8e5a8baa01f6435146ec01
mitesh <98982812+mitscodes@users.noreply.github.com>
2026-08-12 18:19:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,13 +19,15 @@ resource "aws_security_group" "example" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
+    description = "Allow SSH from internal network only"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+    description = "Allow HTTPS to internal network only"
   }
 }
